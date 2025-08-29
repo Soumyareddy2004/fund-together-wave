be8b3bff86c62f7d06169c79180c5f2cd81cd4bf
Soumya Bodigam <sowmyareddy1911@gmail.com>
2025-08-29 11:30:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/fund-together-wave/", // 👈 VERY IMPORTANT for GitHub Pages
+  base: "/fund-together-wave/", // 👈 ADD THIS
   server: {
     host: "::",
     port: 8080,
